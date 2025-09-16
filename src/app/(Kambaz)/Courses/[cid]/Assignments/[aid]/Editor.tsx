2260d74af2eb8939aa280bacd79856742ad6f7a4
carynd <caryntech@gmail.com>
2025-09-16 07:18:36 -0400

    Initial commit

---
+++ src/app/(Kambaz)/Courses/[cid]/Assignments/[aid]/Editor.tsx
@@ -0,0 +1,8 @@
+import { redirect } from "next/navigation";
+
+
+export default async function CoursesPage({ params, }: { params: Promise<{ aid: string }>; }) {
+ const { aid } = await params;
+ redirect(`/Courses/${aid}/Home`);
+}
+
