bcca7d11121ad4c2e6046754d65386a0232b47a3
carynd <caryntech@gmail.com>
2025-10-19 10:43:47 -0400

    spacing issue

--- src/app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ src/app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client"
 import { useParams } from "next/navigation";
 import * as db from "../../../Database";
 import { ListGroup } from "react-bootstrap";
 import { BsGripVertical } from "react-icons/bs";
+import ModulesControls from "./ModulesControls";
 import ModuleControlButtons from "./ModuleControlButtons";
 import LessonControlButtons from "./LessonControlButtons";
 
@@ -11,36 +13,40 @@ export default function Modules() {
   const modules = db.modules;
 
   return (
-    <ListGroup id="wd-modules" className="rounded-0">
-      {modules
-        .filter((module: any) => module.course === cid)
-        .map((module: any) => (
-          <ListGroup.Item
-            key={module._id}
-            className="wd-module p-0 mb-5 fs-5 border-gray"
-          >
-            <div className="wd-title p-3 ps-2 bg-secondary">
-              <BsGripVertical className="me-2 fs-3" />
-              {module.name}
-              <ModuleControlButtons />
-            </div>
-            {module.lessons && (
-              <ListGroup className="wd-lessons rounded-0">
-                {module.lessons.map((lesson: any) => (
-                  <ListGroup.Item
-                    key={lesson._id}
-                    className="wd-lesson p-3 ps-1"
-                  >
-                    <BsGripVertical className="me-2 fs-3" />
-                    {lesson.name}
-                    <LessonControlButtons />
-                  </ListGroup.Item>
-                ))}
-              </ListGroup>
-            )}
-          </ListGroup.Item>
-        ))
-      }
-    </ListGroup>
+    <div>
+      <ModulesControls />
+      <br /><br /><br /><br />
+      <ListGroup id="wd-modules" className="rounded-0">
+        {modules
+          .filter((module: any) => module.course === cid)
+          .map((module: any) => (
+            <ListGroup.Item
+              key={module._id}
+              className="wd-module p-0 mb-5 fs-5 border-gray"
+            >
+              <div className="wd-title p-3 ps-2 bg-secondary">
+                <BsGripVertical className="me-2 fs-3" />
+                {module.name}
+                <ModuleControlButtons />
+              </div>
+              {module.lessons && (
+                <ListGroup className="wd-lessons rounded-0">
+                  {module.lessons.map((lesson: any) => (
+                    <ListGroup.Item
+                      key={lesson._id}
+                      className="wd-lesson p-3 ps-1"
+                    >
+                      <BsGripVertical className="me-2 fs-3" />
+                      {lesson.name}
+                      <LessonControlButtons />
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          ))
+        }
+      </ListGroup>
+    </div>
   );
 }
